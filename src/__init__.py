dac15de901389fb61203a66a7df04cbed68b1b1f
Malte <malte.walkowiak@hdi.de>
2023-01-30 15:28:03 +0100

    [FIX] Revised repo import

---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from .s3_sample import s3_bucket_sample as s3_sample_repository
--- src/s3_sample/s3_bucket_sample/schedules/load_s3_schedule.py
+++ src/s3_sample/s3_bucket_sample/schedules/load_s3_schedule.py
@@ -3,7 +3,7 @@ from dagster import DefaultScheduleStatus, ScheduleDefinition
 from ..jobs.load_s3_job import load_s3
 
 load_s3_schedule = ScheduleDefinition(
-    name="daily_compute_cereal_properties",
+    name="s3_sample_routine",
     cron_schedule="0 0 * * *",
     job=load_s3,
     default_status=DefaultScheduleStatus.RUNNING,
